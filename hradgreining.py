640745518bd6ede06f004ca5c86c6b251d12f033
nuha samson <146358815+nunu50@users.noreply.github.com>
2024-08-07 18:44:05 +0300

    Add files via upload

---
+++ hradgreining.py
@@ -0,0 +1,5 @@
+n = str(input())
+if 'COV' in n:
+    print("Veikur!")
+else:
+    print("Ekki veikur!")
---
+++ storafmaeli.py
@@ -0,0 +1,5 @@
+n = int(input())
+if n%10 == 0:
+    print("Jebb")
+else:
+    print("Neibb")
