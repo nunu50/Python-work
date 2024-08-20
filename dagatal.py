8b1345f37f92a5262ffb3512052b981b1f83cafc
nuha samson <146358815+nunu50@users.noreply.github.com>
2024-08-20 19:35:52 +0300

    Add files via upload

---
+++ dagatal.py
@@ -0,0 +1,7 @@
+m = int(input())
+if m == 1 or m == 3 or m == 5 or m == 7 or m == 8  or m == 10 or m == 12 :
+    print(31)
+elif m == 2:
+    print(28)
+elif m == 4 or m == 6 or m == 11 or m == 9:
+    print(30)
